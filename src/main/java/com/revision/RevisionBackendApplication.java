461f8b031a1d03ead2c26b91e1c8c240041c4280
PRIYA-L1305 <priya.loganathan1305@gmail.com>
2026-02-01 16:48:46 +0530

    Commit in creating revision project and testcontroller for testing backend.

---
+++ src/main/java/com/revision/RevisionBackendApplication.java
@@ -0,0 +1,14 @@
+package com.revision;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RevisionBackendApplication {
+
+    public static void main(String[] args) {
+
+        SpringApplication.run(RevisionBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/revision/revisionbackend/RevisionBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.revision.revisionbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RevisionBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
