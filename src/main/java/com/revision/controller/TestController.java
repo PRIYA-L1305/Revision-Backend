a4f03479af3d562f0318e9f7c61bd1c437635624
PRIYA-L1305 <priya.loganathan1305@gmail.com>
2026-02-02 09:06:04 +0530

    Commit in adding topic model and get post methods in controller

--- src/main/java/com/revision/controller/TestController.java
+++ src/main/java/com/revision/controller/TestController.java
@@ -1,8 +1,10 @@
 package com.revision.controller;
 
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.revision.model.Topic;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @CrossOrigin(origins ={"http://localhost:5500",
         "http://127.0.0.1:5500",
@@ -10,8 +12,20 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class TestController {
 
+    private List<Topic> topics = new ArrayList<>();
+
     @GetMapping("/hello")
     public String hello() {
         return "Backend is running!";
+    }
+
+    @PostMapping("/topics")
+    public void addTopic(@RequestBody Topic topic) {
+        topics.add(topic);
+    }
+
+    @GetMapping("/topics")
+    public List<Topic> getTopics() {
+        return topics;
     }
 }
