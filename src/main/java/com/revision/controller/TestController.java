515f1c6ef490c76badd7d981b75f29abe3d36806
PRIYA-L1305 <priya.loganathan1305@gmail.com>
2026-02-02 08:46:03 +0530

    Commit in updating crossorigin for frontend.

--- src/main/java/com/revision/controller/TestController.java
+++ src/main/java/com/revision/controller/TestController.java
@@ -1,8 +1,12 @@
 package com.revision.controller;
 
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@CrossOrigin(origins ={"http://localhost:5500",
+        "http://127.0.0.1:5500",
+        "https://revision-frontend-app.netlify.app/"})
 @RestController
 public class TestController {
 
